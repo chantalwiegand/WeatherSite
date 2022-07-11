a1c29877cff19f581f3130826bc3b8d93967dfac
ChantalWiegand1 <107423184+ChantalWiegand1@users.noreply.github.com>
2022-07-11 12:47:26 +0200

    Setup api call (missing api key)

--- Website/weather.js
+++ Website/weather.js
@@ -1,1 +1,9 @@
 "use strict"
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Drempt&appid={API key}"
+$.ajax({
+    url: url,
+    succes: function(result){
+        console.log(result);
+    }
+})
