cace33cc056e1cd99d80f30af9fdf601bf9230fa
ChantalWiegand1 <107423184+ChantalWiegand1@users.noreply.github.com>
2022-07-11 13:42:59 +0200

    finish api all

--- Website/weather.js
+++ Website/weather.js
@@ -1,9 +1,19 @@
 "use strict"
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Drempt&appid={API key}"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Drempt&appid=3b432d76373309b27d4048c1b4bb0c83"
 $.ajax({
     url: url,
-    succes: function(result){
-        console.log(result);
+    success: function (result) {
+        $("#location").text(result.name);
+
+        let C = Math.round(result.main.temp - 273);
+        let Celcius = C.toString();
+        $("#temp").text(Celcius);
+
+        let windSpeed = Math.round(result.wind.speed * 3.6)
+        let wind = windSpeed.toString();
+        $("#wind").text(wind);
+
+        $("#sky").text(result.weather[0].description);
     }
 })
